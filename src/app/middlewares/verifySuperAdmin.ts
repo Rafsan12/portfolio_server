a8c5d34195b029901a4e4dadff5dfb10df91f8a0
Rafsan12 <rafsan.web.2022@gmail.com>
2025-10-04 15:18:38 +0600

    verifySuperAdmin change

--- src/app/middlewares/verifySuperAdmin.ts
+++ src/app/middlewares/verifySuperAdmin.ts
@@ -1,11 +1,16 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prisma } from "../../config/db";
 
-export const verifySuperAdmin = async (req: Request, res: Response) => {
+export const verifySuperAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { userId } = req.body;
   const user = await prisma.user.findUnique({ where: { id: userId } });
 
   if (!user) return res.status(401).json({ message: "User not found" });
   if (user.role !== "SUPER_ADMIN")
     return res.status(403).json({ message: "Access denied" });
+  next();
 };
