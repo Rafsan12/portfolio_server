02c3b1b0c7a08320ba2f446601eed3b993e7d80a
Rafsan12 <rafsan.web.2022@gmail.com>
2025-10-04 14:18:37 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 app.use("/api", router);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Api is running");
